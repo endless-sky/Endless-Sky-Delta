0d633f6eb3b0e6e41e8fe51a5aec65b538013c4d
UnorderedSigh <116329264+UnorderedSigh@users.noreply.github.com>
2023-06-02 01:28:50 -0400

    Limit number of fleets & spawn specified number when entering system

--- source/Ship.h
+++ source/Ship.h
@@ -48,6 +48,7 @@ class Phrase;
 class Planet;
 class PlayerInfo;
 class Projectile;
+class SpawnedFleet;
 class StellarObject;
 class System;
 class Visual;
@@ -472,6 +473,10 @@ public:
 	std::shared_ptr<Ship> GetParent() const;
 	const std::vector<std::weak_ptr<Ship>> &GetEscorts() const;
 
+	std::shared_ptr<SpawnedFleet> GetSpawnedFleet();
+	std::shared_ptr<const SpawnedFleet> GetSpawnedFleet() const;
+	void SetSpawnedFleet(std::shared_ptr<SpawnedFleet> fleet);
+
 
 private:
 	// Add or remove a ship from this ship's list of escorts.
@@ -514,6 +519,9 @@ private:
 	EsUuid uuid;
 	std::string name;
 	bool canBeCarried = false;
+
+	// If this ship is part of a spawned fleet:
+	std::shared_ptr<SpawnedFleet> spawnedFleet;
 
 	int forget = 0;
 	bool isInSystem = true;
