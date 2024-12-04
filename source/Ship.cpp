06d1945149836e8801162e8d43636a492b70cebe
MadisonianX <madisonianx@gmail.com>
2024-12-04 06:36:20 -0600

    add intrasolar attribute and update flight check for no bays
    Adds an intrasolar attribute that can be used for Fighters to allow their use in-system without a Hyperdrive or Insufficient Bays flight check warning on depart.

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -1360,6 +1360,7 @@ vector<string> Ship::FlightCheck() const
 	double turnEnergy = attributes.Get("turning energy");
 	double hyperDrive = navigation.HasHyperdrive();
 	double jumpDrive = navigation.HasJumpDrive();
+	double intrasolar = attributes.Get("intrasolar");
 
 	// Report the first error condition that will prevent takeoff:
 	if(IdleHeat() >= MaximumHeat())
@@ -1392,6 +1393,13 @@ vector<string> Ship::FlightCheck() const
 			checks.emplace_back("solar power?");
 		if(fuel < 0.)
 			checks.emplace_back("fuel?");
+		if(canBeCarried && !intrasolar)
+		{
+			if(!hyperDrive && !jumpDrive)
+				checks.emplace_back("no hyperdrive?");
+			if(fuelCapacity < navigation.JumpFuel())
+				checks.emplace_back("no fuel?");
+		}
 		if(!canBeCarried)
 		{
 			if(baseAttributes.Category() != "Intrasolar" && (!hyperDrive && !jumpDrive))
