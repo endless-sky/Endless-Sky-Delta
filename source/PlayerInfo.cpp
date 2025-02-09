707cd14b9b4ba9ed9535c3eeb472fff0223e73f4
Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>
2025-02-09 08:50:13 +0300

    Update source/PlayerInfo.cpp

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -767,11 +767,6 @@ void PlayerInfo::AdvanceDate(int amount)
 	for(const shared_ptr<Ship> &ship : ships)
 		assets += ship->Cargo().Value(system);
 
-	// Have the player pay salaries, mortgages, etc. and print a message that
-	// summarizes the payments that were made.
-	string message = accounts.Step(assets, Salaries(), b.maintenanceCosts);
-	if(!message.empty())
-		Messages::Add(message, Messages::Importance::High);
 
 		DoAccounting();
 	}
