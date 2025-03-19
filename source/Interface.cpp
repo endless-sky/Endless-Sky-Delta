569b4c608fbf785c80b047479e0321c3aeaf630c
Zitchas <32169904+Zitchas@users.noreply.github.com>
2025-03-19 06:38:06 -0400

    Tidying up to match the other section

--- source/Interface.cpp
+++ source/Interface.cpp
@@ -840,13 +840,14 @@ void Interface::BarElement::Draw(const Rectangle &rect, const Information &info,
 
 				double lim = max(v, value);
 				Point to = start + lim * dimensions;
-
+				Color nToColor = Color::Combine(1 - lim, *fromColor, lim, *toColor);
+				v -= empty;
+				
+				// Rounded lines have a bit of padding, so account for that here.
 				float d = (to - from).Length() / 2.;
 				float twidth = d < width ? width * d / 2. : width;
 				from += unit * twidth;
 				to -= unit * twidth;
-				Color nToColor = Color::Combine(1 - lim, *fromColor, lim, *toColor);
-				v -= empty;
 
 				LineShader::DrawGradient(from, to, twidth, nFromColor, nToColor);
 			}
